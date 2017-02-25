c4a2cb1645f19376e1855319e6a2157510a10103
saki4510t <saki4510t@gmail.com>
2017-02-25 18:00:44 +0900

    VoiceFeedbackの音声データ初期化用のクラスを追加。MainActivityから呼び出す

--- voiceparrot/src/main/java/com/serenegiant/voiceparrot/MainActivity.java
+++ voiceparrot/src/main/java/com/serenegiant/voiceparrot/MainActivity.java
@@ -1,6 +1,7 @@
 package com.serenegiant.voiceparrot;
 
 import android.app.Fragment;
+import android.os.Bundle;
 
 import com.serenegiant.aceparrot.AbstractMainActivity;
 
@@ -9,6 +10,12 @@ public class MainActivity extends AbstractMainActivity {
 	// Fragmentが切り替わらずに処理中にもかかわらずActivityが破棄されてしまう
 	private static final boolean DEBUG = false;    // FIXME 実働時はfalseにすること
 	private static String TAG = MainActivity.class.getSimpleName();
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		VoiceFeedbackSettings.init();
+	}
 
 	protected Fragment createConnectionFragment() {
 		return MyInstructionsFragment.newInstance();
---
+++ voiceparrot/src/main/java/com/serenegiant/voiceparrot/VoiceFeedbackSettings.java
@@ -0,0 +1,11 @@
+package com.serenegiant.voiceparrot;
+
+/**
+ * Created by saki on 2017/02/25.
+ *
+ */
+public class VoiceFeedbackSettings {
+
+	public static void init() {
+	}
+}
