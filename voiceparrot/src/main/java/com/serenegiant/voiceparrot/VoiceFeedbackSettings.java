0670b9f248b352b6d1c6fcd39280fa05fff15ed6
saki4510t <saki4510t@gmail.com>
2017-02-25 18:02:20 +0900

    VoiceFeedbackの音声データのマップを参照できた^^

--- voiceparrot/src/main/java/com/serenegiant/voiceparrot/VoiceFeedbackSettings.java
+++ voiceparrot/src/main/java/com/serenegiant/voiceparrot/VoiceFeedbackSettings.java
@@ -1,5 +1,8 @@
 package com.serenegiant.voiceparrot;
 
+import com.serenegiant.aceparrot.VoiceFeedback;
+import com.serenegiant.utils.CollectionMap;
+
 /**
  * Created by saki on 2017/02/25.
  *
@@ -7,5 +10,6 @@ package com.serenegiant.voiceparrot;
 public class VoiceFeedbackSettings {
 
 	public static void init() {
+		final CollectionMap<Long, Integer> ids = VoiceFeedback.ID_MAP;
 	}
 }
